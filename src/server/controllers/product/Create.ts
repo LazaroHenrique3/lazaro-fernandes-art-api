dbef20f23bb7d84d80894a34ee58ff5d11b09b70
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-08-04 14:43:49 -0300

    feat: Ajustando a tipagem de validação do product

--- src/server/controllers/product/Create.ts
+++ src/server/controllers/product/Create.ts
@@ -3,21 +3,25 @@ import { StatusCodes } from 'http-status-codes'
 import * as yup from 'yup'
 
 import { validation } from '../../shared/middleware'
-import { IProduct, IImageObject } from '../../database/models'
+import { IProductFile } from '../../database/models'
 import { ProductProvider } from '../../database/providers/product'
 
 //Para tipar o body do request
-interface IBodyProps extends Omit<IProduct, 'id'> { }
+interface IBodyProps extends Omit<IProductFile, 'id'> { }
+
+interface IValidateProps extends Omit<IProductFile, 'id' | 'main_image' | 'product_images'> { }
 
 //tipando o files
-interface IFilesProps {
-    main_image: IImageObject[]
-    product_images: IImageObject[]
+interface IFilesValidateProps {
+    main_image: Express.Multer.File[]
+    product_images: Express.Multer.File[]
 }
+
+const MAX_PRODUCT_IMAGES = 4
 
 //Midleware
 export const createValidation = validation((getSchema) => ({
-    body: getSchema<IBodyProps>(yup.object().shape({
+    body: getSchema<IValidateProps>(yup.object().shape({
         status: yup.string().oneOf(['Ativo', 'Vendido', 'Inativo']).required(),
         title: yup.string().required().min(1).max(100),
         orientation: yup.string().oneOf(['Retrato', 'Paisagem']).required(),
@@ -57,109 +61,68 @@ export const createValidation = validation((getSchema) => ({
         description: yup.string().optional(),
         weight: yup.number().moreThan(0).required(),
         price: yup.number().moreThan(0).required(),
-        main_image: yup.mixed().required().default([]),
-        product_images: yup.array().required().default([]),
         dimension_id: yup.number().moreThan(0).required(),
         technique_id: yup.number().moreThan(0).required(),
         category_id: yup.number().moreThan(0).required(),
     })),
-    files: getSchema<IFilesProps>(yup.object().shape({
-        main_image: yup.array()
-            .test('isImage', 'A imagem principal é obrigatória!', (value) => {
-                const mainImage: IImageObject[] = value as IImageObject[]
+    files: getSchema<IFilesValidateProps>(yup.object().shape({
+        main_image: yup.mixed()
+            .test('isImage', (value) => {
+                const mainImage: Express.Multer.File[] = value as Express.Multer.File[]
 
+
+                //Verificando se foi passado imagem
                 if (mainImage.length === 0) {
-                    return false
+                    throw new yup.ValidationError('A imagem é obrigatória!', value, 'main_image')
                 }
 
-                return true
-            })
-            .required()
-            .default([])
-            .test('fileType', 'Formato de imagem inválido', (value) => {
-                const mainImage: IImageObject[] = value as IImageObject[]
-
-                if (mainImage.length === 0) {
-                    return false
-                }
-
-                //Verificando o formato
+                //Verificando o formato das imagens
                 const supportedFormats = ['image/jpeg', 'image/png', 'image/jpg']
                 if (!supportedFormats.includes(mainImage[0].mimetype)) {
-                    return false
-                }
-
-                return true
-            })
-            .required()
-            .default([])
-            .test('fileSize', 'Tamanho de imagem excede 2MB', (value) => {
-                const mainImage: IImageObject[] = value as IImageObject[]
-
-                if (mainImage.length === 0) {
-                    return false
+                    throw new yup.ValidationError('Formato de imagem inválido!', value, 'main_image')
                 }
 
                 // Verifica se o tamanho da imagem é maior que 2MB (em bytes)
                 const maxSize = 2 * 1024 * 1024 // 2MB
                 if (Number(mainImage[0].size) > maxSize) {
-                    return false
+                    throw new yup.ValidationError('Tamanho de imagem excede 2MB!', value, 'main_image')
                 }
 
                 return true
             })
-            .required()
-            .default([]),
-        product_images: yup.array()
-            .test('isImage', 'As imagens do produto são obrigatórias!', (value) => {
-                const producImages: IImageObject[] = value as IImageObject[]
+            .required() as yup.Schema<Express.Multer.File[]>,
+        product_images: yup.mixed()
+            .test('isImage', (value) => {
+                const product_images: Express.Multer.File[] = value as Express.Multer.File[]
 
-                if (producImages.length === 0) {
-                    return false
+                //Verificando se foi passado imagem e se é mais que  o permitido
+                if (product_images.length === 0) {
+                    throw new yup.ValidationError('As imagens são obrigatórias!', value, 'product_images')
+                } else if (product_images.length > MAX_PRODUCT_IMAGES) {
+                    throw new yup.ValidationError(`É permitido o upload de até ${MAX_PRODUCT_IMAGES} imagens!`, value, 'product_images')
                 }
 
-                return true
-            })
-            .required()
-            .default([])
-            .test('moreThan4Images', 'É permitido o upload de até 4 imagens!', (value) => {
-                const producImages: IImageObject[] = value as IImageObject[]
-
-                if (producImages.length > 4) {
-                    return false
-                }
-
-                return true
-            })
-            .required()
-            .default([])
-            .test('fileType', 'Formato de imagem inválido', (value) => {
-                const productImages: IImageObject[] = value as IImageObject[]
-
-                if (productImages.length === 0) {
-                    return false
-                }
-
-                //Verificando o formato
+                //Verificando o formato das imagens
                 const supportedFormats = ['image/jpeg', 'image/png', 'image/jpg']
-                return productImages.every((item) => supportedFormats.includes(item.mimetype))
-
-            })
-            .required()
-            .default([])
-            .test('fileSize', 'Tamanho de imagem excede 2MB', (value) => {
-                const productImages: IImageObject[] = value as IImageObject[]
-
-                if (productImages.length === 0) {
-                    return false
+                for (let i = 0; i < product_images.length; i++) {
+                    const image = product_images[i]
+                    if (!supportedFormats.includes(image.mimetype)) {
+                        throw new yup.ValidationError('Formato de imagem inválido!', value, 'product_images')
+                    }
                 }
 
                 // Verifica se o tamanho da imagem é maior que 2MB (em bytes)
                 const maxSize = 2 * 1024 * 1024 // 2MB
-                return productImages.every((item) => Number(item.size) <= maxSize)
+                for (let i = 0; i < product_images.length; i++) {
+                    const image = product_images[i]
+                    if (Number(image.size) > maxSize) {
+                        throw new yup.ValidationError('Tamanho de imagem excede 2MB!', value, 'product_images')
+                    }
+                }
+
+                return true
             })
-            .required()
-            .default([]),
+            .required() as yup.Schema<Express.Multer.File[]>,
     }))
 }))
 
@@ -167,7 +130,7 @@ export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
     //Pegando as imagens enviadas do frontend
     const { main_image, product_images } = req.files as { product_images?: Express.Multer.File[], main_image?: Express.Multer.File[] }
 
-    if(main_image === undefined || product_images === undefined){
+    if (main_image === undefined || product_images === undefined) {
         return res.status(StatusCodes.BAD_REQUEST).json({
             errors: {
                 default: 'Imagens não foram passadas corretamente!.'
@@ -175,7 +138,7 @@ export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
         })
     }
 
-    const result = await ProductProvider.create({ ...req.body, status: 'Ativo', main_image, product_images})
+    const result = await ProductProvider.create({ ...req.body, status: 'Ativo', main_image, product_images })
 
     if (result instanceof Error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
--- src/server/database/models/Product.ts
+++ src/server/database/models/Product.ts
@@ -1,3 +1,20 @@
+export interface IProductFile {
+    id: number
+    status: 'Ativo' | 'Vendido' | 'Inativo'
+    title: string
+    orientation: 'Retrato' | 'Paisagem'
+    quantity: number
+    production_date: Date | string
+    description?: string
+    weight: number
+    price: number
+    main_image: Express.Multer.File[]
+    product_images: Express.Multer.File[]
+    dimension_id: number
+    technique_id: number
+    category_id: number
+}
+
 export interface IProduct {
     id: number
     status: 'Ativo' | 'Vendido' | 'Inativo'
